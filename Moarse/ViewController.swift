662a727a951c9f20b0364144e6168e53a77aa8e8
Padraig <padraig.kennedy@gmail.com>
2021-10-31 12:41:18 -0700

    Initial Commit

---
+++ Moarse/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Moarse
+//
+//  Created by Padraig O Cinneide on 2021-10-31.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
